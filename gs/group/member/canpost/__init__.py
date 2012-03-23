9cdd3481b50c2350d451cf6bf9e471a471a26476
Michael JasonSmith <mpj17@onlinegroups.net>
2012-03-23 15:14:33 +1100

    More robust notifications, so the mailing-list can send them.

--- gs/group/member/canpost/__init__.py
+++ gs/group/member/canpost/__init__.py
@@ -1,3 +1,5 @@
 # coding=utf-8
 from viewlet import RuleViewlet
+from interfaces import IGSPostingUser
+from notifier import Notifier
 
--- gs/group/member/canpost/notifier.py
+++ gs/group/member/canpost/notifier.py
@@ -11,7 +11,7 @@ from gs.profile.notify.sender import MessageSender
 from interfaces import IGSPostingUser
 UTF8 = 'utf-8'
 
-class NotifyNewAdmin(object):
+class Notifier(object):
     textTemplateName = 'cannot-post.txt'
     htmlTemplateName = 'cannot-post.html'
     
@@ -39,39 +39,38 @@ class NotifyNewAdmin(object):
         assert retval
         return retval
         
-    def notify(self, userInfo, siteInfo, groupInfo):
+    def notify(self, userInfo, siteInfo, groupInfo, origMesg):
         subject = (u'%s: Problem Posting' % groupInfo.name).encode(UTF8)
         text = self.textTemplate(userInfo=userInfo, siteInfo=siteInfo, 
                     groupInfo=groupInfo)
         html = self.htmlTemplate(userInfo=userInfo, siteInfo=siteInfo, 
                     groupInfo=groupInfo)
-        ms = MessageSender(self.context, userInfo)
-        ms.send_message(subject, text, html)
+                    
+        #ms = MessageSender(self.context, userInfo)
+        #ms.send_message(subject, text, html)
 
 class CannotPostMessage(GroupPage):
-    @Lazy
-    def supportAddress(self):
+    def supportAddress(self, userInfo):
         gn = self.groupInfo.name.encode('ascii', 'ignore')
         s = 'Subject=%s' % quote('Cannot Post to %s' % gn)
-        b = 'body=%s' % quote(self.messageBody)
+        b = 'body=%s' % quote(self.message_body(userInfo))
         e = get_support_email(self.context, self.siteInfo.id)
         retval = 'mailto:%s?%s&%s' % (e, b, s)
         return retval
     
-    @Lazy
-    def messageBody(self):
+    def message_body(self, userInfo):
         gn = self.groupInfo.name.encode('ascii', 'ignore')
         sn = self.siteInfo.name.encode('ascii', 'ignore')
+        cp  = self.can_post_for_user(userInfo)
         m = 'I had a problem sending an email to %s on %s <%s>. '\
-            'The issue was "%s."' % \
-            (gn, sn, self.groupInfo.url, self.canPost.status)
+            'The issue was "%s (Reason Number %s)"' % \
+            (gn, sn, self.groupInfo.url, cp.status, cp.statusNum)
         retval = 'Hi!\n\n%s\n\nI need your help because...' % TextWrapper().fill(m)
         return retval
     
-    @Lazy
-    def canPost(self):
+    def can_post_for_user(self, userInfo):
         group = self.groupInfo.groupObj
-        retval = getMultiAdapter((group, self.loggedInUserInfo), 
+        retval = getMultiAdapter((group, userInfo),
                     IGSPostingUser)
         return retval
 
