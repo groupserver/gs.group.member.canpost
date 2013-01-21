addb9e3e94db7128ca06d826a2d85bee06fefb14
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-22 10:07:23 +1100

    Minor code cleanup, thanks to Ninja IDE.

--- gs/group/member/canpost/canpost.py
+++ gs/group/member/canpost/canpost.py
@@ -1,44 +1,42 @@
 # coding=utf-8
 from operator import and_
 from zope.cachedescriptors.property import Lazy
-from zope.component import adapts, getGlobalSiteManager, getAdapters
-from zope.interface import implements, providedBy
-from Products.CustomUserFolder.interfaces import IGSUserInfo
-from gs.group.member.canpost.interfaces import IGSPostingUser, IGSCanPostRule
+from zope.component import getGlobalSiteManager
+from gs.group.member.canpost.interfaces import IGSCanPostRule
+
 
 class CanPostToGroup(object):
     def __init__(self, group, userInfo):
         self.group = group
         self.userInfo = userInfo
-    
+
     @Lazy
     def adaptors(self):
         gsm = getGlobalSiteManager()
-        retval = [a for a in gsm.getAdapters((self.userInfo, self.group), 
+        retval = [a for a in gsm.getAdapters((self.userInfo, self.group),
                                               IGSCanPostRule)]
-        retval.sort(key=lambda r:r[1].weight)
+        retval.sort(key=lambda r: r[1].weight)
         return retval
 
     @Lazy
     def rules(self):
         retval = [instance for name, instance in self.adaptors]
         return retval
-    
+
     @Lazy
     def canPost(self):
         return reduce(and_, [rule.canPost for rule in self.rules], True)
-    
+
     @Lazy
     def statusNum(self):
-        statusNums = [rule.statusNum for rule in self.rules 
+        statusNums = [rule.statusNum for rule in self.rules
                         if rule.statusNum != 0]
         retval = (statusNums and min(statusNums)) or 0
         assert retval >= 0
         return retval
-    
+
     @Lazy
     def status(self):
-        retval = [rule.status for rule in self.rules 
+        retval = [rule.status for rule in self.rules
                     if rule.statusNum == self.statusNum][0]
         return retval
-
