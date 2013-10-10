38d515d5ca28e5fdbf6c2d06526348604152283a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-09 17:53:45 -0700

    Using absolute import

--- gs/group/member/canpost/__init__.py
+++ gs/group/member/canpost/__init__.py
@@ -1,6 +1,8 @@
-# coding=utf-8
-from viewlet import RuleViewlet
-from interfaces import IGSPostingUser
-from notifier import Notifier
-from unknownemail import Notifier as UnknownEmailNotifier
-
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+#lint:disable
+from .viewlet import RuleViewlet
+from .interfaces import IGSPostingUser
+from .notifier import Notifier
+from .unknownemail import Notifier as UnknownEmailNotifier
+#lint:enable
