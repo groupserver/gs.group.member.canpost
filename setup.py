651efe97cf1c7db3b6dbf576d9c5693042c852e3
Michael JasonSmith <mpj17@onlinegroups.net>
2015-01-14 16:23:49 -0800

    PEP-8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -11,62 +11,67 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.group.member.canpost',
-    version=version,
-    description="Determine if a group member can post.",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        "Environment :: Web Environment",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+      version=version,
+      description="Determine if a group member can post.",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='groupserver member post group private secret',
-    author='Michael JasonSmith',
-    author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org/',
-    license='ZPL 2.1',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.member'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'setuptools',
-        'pytz',
-        'zope.app.apidoc',
-        'zope.app.pagetemplate',
-        'zope.cachedescriptors',
-        'zope.component',
-        'zope.contentprovider',
-        'zope.schema',
-        'zope.viewlet',
-        'gs.content.email.base',
-        'gs.content.email.layout',
-        'gs.core',
-        'gs.database',
-        'gs.group.base',
-        'gs.group.privacy',
-        'gs.profile.email.base',
-        'gs.profile.notify',
-        'gs.viewlet',
-        'Products.CustomUserFolder',
-        'Products.GSGroup',
-        'Products.XWFCore',
-        'Products.GSProfile',
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,)
+      keywords='groupserver member post group private secret',
+      author='Michael JasonSmith',
+      author_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/gs.group.member.canpost',
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gs', 'gs.group', 'gs.group.member'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'pytz',
+          'zope.app.apidoc',
+          'zope.app.pagetemplate',
+          'zope.cachedescriptors',
+          'zope.component',
+          'zope.contentprovider',
+          'zope.schema',
+          'zope.viewlet',
+          'gs.content.email.base',
+          'gs.content.email.layout',
+          'gs.core',
+          'gs.database',
+          'gs.group.base',
+          'gs.group.privacy',
+          'gs.profile.email.base',
+          'gs.profile.notify',
+          'gs.viewlet',
+          'Products.CustomUserFolder',
+          'Products.GSGroup',
+          'Products.XWFCore',
+          'Products.GSProfile',
+      ],
+      entry_points="""# -*- Entry points: -*-
+        """,)
